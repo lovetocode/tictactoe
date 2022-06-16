544d582c1c5955474a72e4bf4827058112b580d8
lovetocode <dshaggy28@yahoo.com>
2022-06-15 18:37:35 -0700

    worked on computer player

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -11,6 +11,9 @@ onready var checker_dict = {
 	"dia two" : [2,4,6]
 }
 
+var possible_win_x = []
+var possible_win_o = []
+
 onready var game_won = preload("res://Scenes/GameWon.tscn")
 var data_store = [] #stores current values in each pos
 var win = false #check for win
@@ -55,6 +58,13 @@ func check_win(pos, letter):
 		if tally == 3:
 			win = true
 			break
+		elif tally == 2:
+			if letter == 'x':
+				possible_win_x.append(key)
+			else:
+				possible_win_o.append(key)
+			tally = 0 
+				
 		else:
 			tally = 0 
 	
@@ -66,7 +76,7 @@ func won_game(win_key): # To make a win more interesting
 	var node = "POS" + String(win_key[1]) # The middle position of the whole key.
 	inst.position = get_main_node().get_node(node).global_position
 	var diff = win_key[2] - win_key[0]
-	get_main_node().add_child(inst)
+	
 	
 	match diff: #equivalent to switch statement.
 		4: # comes win key diagonal
@@ -75,9 +85,65 @@ func won_game(win_key): # To make a win more interesting
 			inst.rotation = deg2rad(45)
 		6: #this is vertical.
 			inst.rotation = deg2rad(90)
+	
+	get_main_node().add_child(inst)
+	
+func play_winning_move():
+	var played_winning_move = false
+	var played_pos = -1
+	var key_to_remove = -1 # sometimes when possible wins are stored they might be taken by other players.
+	
+	
+	# all possible win outcomes are stored in possible_win_o array
+	if possible_win_o.size() > 0: #this means there is a winning possibility
+		for i in range(0, possible_win_o.size()):
+			# go through all these possibilites
+			for j in range(0, checker_dict[possible_win_o[i]].size()):
+				# go through all the positions in these possibilities
+				if data_store[checker_dict[possible_win_o[i]][j]] == "--":
+					#if a possible possition is empty
+					played_pos = checker_dict[possible_win_o[i]][j] #what should be the possition to play
+					key_to_remove = i
+					# find the node for the position to play
+					var node = "POS" + String(played_pos)
+					get_main_node().get_node(node).play_o()
+					played_winning_move = true
+				if played_winning_move:
+					return played_winning_move
+					
+			if key_to_remove != -1:
+				possible_win_o.remove(key_to_remove)
+			else:
+				possible_win_o = []
+	
+	return played_winning_move # in case its false
+					
+					
+		
+	
+func play_computer():
+	var won_by_computer = play_winning_move() # first priority
+	if won_by_computer:
+		return          #game ends
+	
+	var blocked_players_win = block_players_win() # second priority
+	if blocked_players_win:
+		return                #no other move needed
+#
+#	var draw = check_for_draw() # third priority
+#	if draw():
+#		return                  #Its a stalemate noting to do
+	
+   #if nothing take a random pos and play
+
 				
 		
 func _process(delta):
 	if Input.is_key_pressed(KEY_ENTER):
+		possible_win_o = []
+		possible_win_x = []
 		get_tree().reload_current_scene()
 		reset_data_store()
+		
+	if Input.is_action_just_pressed("ui_select"): # for testing press space
+		play_computer()
